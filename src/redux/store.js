0953faae02e9eb1fc680e19d05674c1d4b374a49
Mykhailo Popyzenko <mp.and.sp@ukr.net>
2023-10-16 22:00:48 +0300

    add folder redux

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { contactsSlice } from './contactSlice';
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactsSlice,
+  },
+});
